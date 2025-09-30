8766dedd45464a9b573ee336897b9b1ea504bb59
Akshay Ladne <Kumawata283@gmail.com>
2025-09-30 23:03:41 +0530

    refactored the code

--- 01. CRUD features/src/CRUD.jsx
+++ 01. CRUD features/src/CRUD.jsx
@@ -1,11 +1,26 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 
 const CRUD = () => {
 
     const [task, setTask] = useState('')
     const [todos, setTodos] = useState([])
+    const [search, setSearch] = useState('')
     const [editingIndex, setEditingIndex] = useState(null);
     const [editValue, setEditValue] = useState('');
+
+
+    useEffect(() => {
+
+        const saveTodos = JSON.parse(localStorage.getItem("todos"))
+        if (saveTodos) {
+            setTodos(saveTodos)
+        }
+
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
 
     // Adding a task
     const handleTasks = (e) => {
@@ -22,64 +37,79 @@ const CRUD = () => {
     }
 
     // Here we are filtering the tasks by searching
-    const filteredTodos = task.trim() === '' ? todos : todos.filter((todo) => todo.toLowerCase().includes(task.toLowerCase()))
+    const filteredTodos = search.trim() === '' ? todos : todos.filter((todo) => todo.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <div className='min-h-screen bg-gray-50'>
             <h1 className='pt-6 text-2xl font-bold text-center'>CRUD APP</h1>
             <div className='flex  items-center flex-col mx-auto 0 max-w-6xl my-12 '>
                 {/* Input field to add tasks */}
-                <div className='flex gap-2 '>
+                <div className='flex gap-2  '>
                     <input value={task} onChange={(e) => setTask(e.target.value)} className='min-w-xl px-4 py-2 outline-none border border-gray-300 rounded-lg shadow-sm' type="text" placeholder='Add something...' />
                     <button onClick={handleTasks} className='px-4 py-2 bg-blue-500 hover:bg-blue-700 hover:cursor-pointer duration-300 ease-in border-none text-white border rounded-lg' type='button'><span className='text-shadow-lg'>Add</span></button>
                 </div>
-
-                {/* Here we rendered the list conditionally */}
+                {/* Search bar for searching */}
                 {todos.length > 0 && (
-                    <div className='mt-12'>
-                        <ul>
-                            {filteredTodos.map((item, index) => (
-                                <li key={index} className='flex justify-between  bg-gray-100 my-2 rounded-lg min-w-lg '>
+                    <div className=' border border-gray-300 rounded-lg px-8 py-6 mt-8'>
+                        <div className="flex gap-2 mt-2 justify-center">
+                            <input
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                className="min-w-xl px-4 py-2 outline-none border border-gray-300 rounded-lg shadow-sm"
+                                type="text"
+                                placeholder="Search tasks..."
+                            />
+                        </div>
 
-                                    {editingIndex === index ? (
-                                        // Input field and buttons for update the tasks
-                                        <div className='flex justify-between min-w-lg'>
-                                            <input value={editValue} onChange={(e) => setEditValue(e.target.value)} className='pl-4 outline-gray-300 border border-gray-300 min-w-md mr-2 rounded-lg' type="text" />
-                                            <div className='flex gap-2'>                                               
-                                                {/* save button */}
-                                                <button onClick={() => {
-                                                    if (editValue.trim() === '') return;
-                                                    const updatedTodos = [...todos]
-                                                    updatedTodos[editingIndex] = editValue;
-                                                    setTodos(updatedTodos)
-                                                    setEditingIndex(null);
-                                                    setEditValue('')
-                                                }} className='text-white bg-green-500 hover:cursor-pointer hover:bg-green-600 px-4 py-2 border-none rounded-lg font-semibold'>Save</button>
-                                                
-                                                {/* cancel button */}
-                                                <button onClick={() => {
-                                                    setEditingIndex(null);
-                                                    setEditValue('');
-                                                }} className='px-4 py-2 bg-red-500 hover:bg-red-700 hover:border-none hover:cursor-pointer duration-200 ease-in border-none rounded-lg text-white font-semibold'>Cancel</button>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                        // Task list
-                                        <div className='flex justify-between  min-w-lg'>                                           
-                                                <span className='text-md pt-2 pl-4 min-w-md mr-2 border border-gray-300 outline-none rounded-lg '>{item}</span>
-                                                <div className='flex gap-2'>
-                                                    <button onClick={() => {
-                                                        setEditingIndex(index)
-                                                        setEditValue(item)
-                                                    }} className='text-white bg-green-500 hover:cursor-pointer hover:bg-green-600 px-4 py-2 border-none rounded-lg font-semibold'>Edit</button>
-                                                    <button onClick={() => handleDelete(index)} className='px-4 py-2 bg-red-500 hover:bg-red-700 hover:border-none hover:cursor-pointer duration-200 ease-in border-none rounded-lg text-white font-semibold' type='button'>Delete</button>
-                                                </div>                                          
-                                        </div>
-                                    )}
-                                </li>
+                        {/* Here we rendered the list conditionally */}
+                        {todos.length > 0 && (
+                            <div className='mt-8'>
+                                <ul>
+                                    {filteredTodos.map((item, index) => (
+                                        <li key={index} className='flex justify-between  bg-gray-100 my-2 rounded-lg min-w-lg '>
 
-                            ))}
-                        </ul>
+                                            {editingIndex === index ? (
+                                                // Input field and buttons for update the tasks
+                                                <div className='flex justify-between min-w-lg'>
+                                                    <input value={editValue} onChange={(e) => setEditValue(e.target.value)} className='pl-4 outline-gray-300 border border-gray-300 min-w-md mr-2 rounded-lg' type="text" />
+                                                    <div className='flex gap-2'>
+                                                        {/* save button */}
+                                                        <button onClick={() => {
+                                                            if (editValue.trim() === '') return;
+                                                            const updatedTodos = [...todos]
+                                                            updatedTodos[editingIndex] = editValue;
+                                                            setTodos(updatedTodos)
+                                                            setEditingIndex(null);
+                                                            setEditValue('')
+                                                        }} className='text-white bg-green-500 hover:cursor-pointer hover:bg-green-600 px-4 py-2 border-none rounded-lg font-semibold'>Save</button>
+
+                                                        {/* cancel button */}
+                                                        <button onClick={() => {
+                                                            setEditingIndex(null);
+                                                            setEditValue('');
+                                                        }} className='px-4 py-2 bg-red-500 hover:bg-red-700 hover:border-none hover:cursor-pointer duration-200 ease-in border-none rounded-lg text-white font-semibold'>Cancel</button>
+                                                    </div>
+                                                </div>
+                                            ) : (
+                                                // Task list
+                                                <div className='flex justify-between  min-w-lg'>
+                                                    <span className='text-md pt-2 pl-4 min-w-md mr-2 border border-gray-300 outline-none rounded-lg '>{item}</span>
+                                                    <div className='flex gap-2'>
+                                                        <button onClick={() => {
+                                                            setEditingIndex(index)
+                                                            setEditValue(item)
+                                                        }} className='text-white bg-green-500 hover:cursor-pointer hover:bg-green-600 px-4 py-2 border-none rounded-lg font-semibold'>Edit</button>
+                                                        <button onClick={() => handleDelete(index)} className='px-4 py-2 bg-red-500 hover:bg-red-700 hover:border-none hover:cursor-pointer duration-200 ease-in border-none rounded-lg text-white font-semibold' type='button'>Delete</button>
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </li>
+
+                                    ))}
+                                </ul>
+                            </div>
+
+                        )}
                     </div>
                 )}
             </div>
